0e5aca48829f0888f873ce7518680574880df5c6
Megan B <brown.m.j@outlook.com>
2017-10-11 15:14:00 -0500

    remove updatedAt and createdAt in models

--- models/department.js
+++ models/department.js
@@ -3,9 +3,7 @@ module.exports = (sequelize, DataTypes) => {
   var department = sequelize.define('department', {
     supervisor_employee_id: DataTypes.INTEGER,
     expense_budget: DataTypes.INTEGER,
-    name: DataTypes.STRING,
-    createdAt: DataTypes.DATE,
-    updatedAt: DataTypes.DATE
+    name: DataTypes.STRING
   });
   /**
    *  employee and departments table have one to many relationship. An employee can be in a single department but a department can have multiple employees. hasMany associates many to one relationship between department and employees.
--- models/employee.js
+++ models/employee.js
@@ -1,28 +1,17 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var employee = sequelize.define(
-    'employee',
-    {
-      first_name: DataTypes.STRING,
-      last_name: DataTypes.STRING,
-      department_id: DataTypes.INTEGER,
-      email: DataTypes.STRING,
-      phone_number: DataTypes.STRING,
-      job_title: DataTypes.STRING,
-      street_address: DataTypes.STRING,
-      city_address: DataTypes.STRING,
-      state_code: DataTypes.STRING,
-      zip_code: DataTypes.INTEGER,
-      createdAt: DataTypes.DATE,
-      updatedAt: DataTypes.DATE
-    }
-    /**
-     * 'Underscored: true' is property added by sequelize when we add a flag --underscored at the time of creation of a model. This property changes the case of default properties(createAt and updateAt) from camel case to snake case(underscore) to match the rest of the property names on the table
-     */
-    // {
-    //   underscored: true
-    // }
-  );
+  var employee = sequelize.define('employee', {
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    department_id: DataTypes.INTEGER,
+    email: DataTypes.STRING,
+    phone_number: DataTypes.STRING,
+    job_title: DataTypes.STRING,
+    street_address: DataTypes.STRING,
+    city_address: DataTypes.STRING,
+    state_code: DataTypes.STRING,
+    zip_code: DataTypes.INTEGER
+  });
   /**
    *  employee and departments table have one to many relationship. An employee can be in a single department but a department can have multiple employees. belongsTo associates one to many relationship here.
    */
