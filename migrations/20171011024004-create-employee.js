c73257b11a588f0fc70b054f150c1911976c031d
thejonroberts <jon.roberts.8@gmail.com>
2017-10-10 22:25:41 -0500

    seed employees table

--- migrations/20171011024004-create-employee.js
+++ migrations/20171011024004-create-employee.js
@@ -1,64 +1,52 @@
 'use strict';
 module.exports = {
 	up: (queryInterface, Sequelize) => {
-		return queryInterface.createTable(
-			'Employees',
-			{
-				id: {
-					allowNull: false,
-					autoIncrement: true,
-					primaryKey: true,
-					type: Sequelize.INTEGER
-				},
-				department_id: {
-					type: Sequelize.INTEGER
-				},
-				email: {
-					type: Sequelize.STRING
-				},
-				first_name: {
-					type: Sequelize.STRING
-				},
-				last_name: {
-					type: Sequelize.STRING
-				},
-				phone_number: {
-					type: Sequelize.STRING
-				},
-				state_code: {
-					type: Sequelize.STRING
-				},
-				street_address: {
-					type: Sequelize.STRING
-				},
-				zip_code: {
-					type: Sequelize.INTEGER
-				},
-				city_address: {
-					type: Sequelize.STRING
-				},
-				// created_at: {
-				// 	type: Sequelize.DATE
-				// },
-				job_title: {
-					type: Sequelize.STRING
-				},
-				// updated_at: {
-				// 	type: Sequelize.DATE
-				// },
-				createdAt: {
-					allowNull: false,
-					type: Sequelize.DATE
-				},
-				updatedAt: {
-					allowNull: false,
-					type: Sequelize.DATE
-				}
+		return queryInterface.createTable('Employees', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
 			},
-			{
-				underscored: true
+			department_id: {
+				type: Sequelize.INTEGER
+			},
+			email: {
+				type: Sequelize.STRING
+			},
+			first_name: {
+				type: Sequelize.STRING
+			},
+			last_name: {
+				type: Sequelize.STRING
+			},
+			phone_number: {
+				type: Sequelize.STRING
+			},
+			state_code: {
+				type: Sequelize.STRING
+			},
+			street_address: {
+				type: Sequelize.STRING
+			},
+			zip_code: {
+				type: Sequelize.INTEGER
+			},
+			city_address: {
+				type: Sequelize.STRING
+			},
+			job_title: {
+				type: Sequelize.STRING
+			},
+			created_at: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			updated_at: {
+				allowNull: false,
+				type: Sequelize.DATE
 			}
-		);
+		});
 	},
 	down: (queryInterface, Sequelize) => {
 		return queryInterface.dropTable('Employees');
