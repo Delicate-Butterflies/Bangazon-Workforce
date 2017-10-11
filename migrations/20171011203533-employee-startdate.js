98693871f8c3f5a3f2086aa4e6310e39a25fca04
Sam <samb283@gmail.com>
2017-10-11 15:47:40 -0500

    migration introduces startdate to employee as needed by issue #2

---
+++ migrations/20171011203533-employee-startdate.js
@@ -0,0 +1,11 @@
+'use strict';
+// To add updated_at attribute to the computers table which was not on boiler plate
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('employees', 'start_date', Sequelize.STRING);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('employees', 'start_date');
+  }
+};
