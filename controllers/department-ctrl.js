44fbb5c21eb0e43361371630a8c49cb8f4b8ae8f
alana.rae.smith@gmail.com <alana.rae.smith@gmail.com>
2017-10-12 15:50:37 -0500

    Created department-detail template/view, began work of getDepartmentById function in controller, and generated route for /departments/:id

--- controllers/department-ctrl.js
+++ controllers/department-ctrl.js
@@ -13,3 +13,32 @@ module.exports.getDepartments = (req, res, next) => {
       next(err); //Ship this nastyness off to our error handler at the bottom of the middleware stack in app.js
     });
 };
+
+module.exports.getDepartmentById = (req, res, next) => {
+  // const { employee, department } = req.app.get('models');
+  const deptEmployees = [
+    {
+      first_name: 'Ali',
+      last_name: 'Smith'
+    },
+    {
+      first_name: 'Carrie',
+      last_name: 'Dayton'
+    }
+  ];
+  res.render('department-detail', { deptEmployees });
+};
+
+module.exports.getEmployeeById = (req, res, next) => {
+  const { employee, department } = req.app.get('models');
+  employee
+    .findById(req.params.id, {
+      include: [{ model: department }]
+    })
+    .then(employee => {
+      res.render('employee-edit', { employee });
+    })
+    .catch(err => {
+      next(err);
+    });
+};
