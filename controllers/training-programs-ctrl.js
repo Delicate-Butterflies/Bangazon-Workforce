9c5f926c68712ac2d07bdfc34da4db9f4b1ff71e
Arwa <arwa.it15@gmail.com>
2017-10-13 11:26:01 -0500

    comments on functions

--- controllers/training-programs-ctrl.js
+++ controllers/training-programs-ctrl.js
@@ -3,7 +3,7 @@
 /** @module TrainingPrograms List Controller */
 
 /**
- * getTrainingProgram function gets all teh training programs that are going to happen in future.
+ * getTrainingProgram function gets all the training programs that are going to happen in future.
  */
 module.exports.getTrainingPrograms = (req, res, next) => {
   const { training_program } = req.app.get('models');
@@ -25,21 +25,26 @@ module.exports.getTrainingPrograms = (req, res, next) => {
     });
 };
 
+/**
+ * postTrainingProgram function saves a training program using a form.
+ */
 module.exports.postTrainingProgram = (req, res, next) => {
   const { training_program } = req.app.get('models');
   const programObj = req.body;
 
   training_program
     .create(programObj)
-    .then(addedProgram => {
-      res.redirect('/training');
+    .then(() => {
+      res.redirect('/training'); // redirects to view training page -- can redirect to single training program view once that function is done
     })
     .catch(err => {
       next(err);
     });
-  // console.log('Added');
 };
 
+/**
+ * getForm function gets the form that will take input to add training programs.
+ */
 module.exports.getForm = (req, res, next) => {
   res.render('program-add');
 };
