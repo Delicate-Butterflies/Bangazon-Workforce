4e2ce485af087ef0b77a140181604e32c34554a8
thejonroberts <jon.roberts.8@gmail.com>
2017-10-13 11:33:56 -0500

    successful rebuild

--- models/department.js
+++ models/department.js
@@ -1,17 +1,17 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var department = sequelize.define('department', {
-    supervisor_employee_id: DataTypes.INTEGER,
-    expense_budget: DataTypes.INTEGER,
-    name: DataTypes.STRING
-  });
-  /**
+	var department = sequelize.define('department', {
+		supervisor_employeeId: DataTypes.INTEGER,
+		expense_budget: DataTypes.INTEGER,
+		name: DataTypes.STRING
+	});
+	/**
    *  employee and departments table have one to many relationship. An employee can be in a single department but a department can have multiple employees. hasMany associates many to one relationship between department and employees.
    */
-  department.associate = function(models) {
-    department.hasMany(models.employee, {
-      foreignKey: 'department_id'
-    });
-  };
-  return department;
+	department.associate = function(models) {
+		department.hasMany(models.employee, {
+			foreignKey: 'departmentId'
+		});
+	};
+	return department;
 };
