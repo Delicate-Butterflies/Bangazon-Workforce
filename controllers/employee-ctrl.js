43e1490d69ca2d17794046f1040eeb7d735f0d63
Sam <samb283@gmail.com>
2017-10-13 10:37:09 -0500

    can now add an employee to the database

--- controllers/employee-ctrl.js
+++ controllers/employee-ctrl.js
@@ -3,38 +3,41 @@
 /* @module Employee List Controller */
 
 module.exports.getEmployees = (req, res, next) => {
-	const { employee, department } = req.app.get('models');
-	employee
-		.findAll({ include: [{ model: department }] })
-		.then(employees => {
-			res.render('employees-list', { employees });
-		})
-		.catch(err => {
-			next(err);
-		});
+  const { employee, department } = req.app.get('models');
+  employee
+    .findAll({ include: [{ model: department }] })
+    .then(employees => {
+      res.render('employees-list', { employees });
+    })
+    .catch(err => {
+      next(err);
+    });
 };
 
 module.exports.getEmployeeById = (req, res, next) => {
-	const { employee, department } = req.app.get('models');
-	employee
-		.findById(req.params.id, {
-			include: [{ model: department }]
-		})
-		.then(employee => {
-			res.render('employee-edit', { employee });
-		})
-		.catch(err => {
-			next(err);
-		});
+  const { employee, department } = req.app.get('models');
+  employee
+    .findById(req.params.id, {
+      include: [{ model: department }]
+    })
+    .then(employee => {
+      res.render('employee-edit', { employee });
+    })
+    .catch(err => {
+      next(err);
+    });
 };
 
 module.exports.showEmployeeForm = (req, res, next) => {
-	const { department } = req.app.get('models');
-	department.findAll().then(departments => {
-		res.render('employee-add', { departments });
-	});
+  const { department } = req.app.get('models');
+  department.findAll().then(departments => {
+    res.render('employee-add', { departments });
+  });
 };
 
 module.exports.addEmployee = (req, res, next) => {
-	// console.log('running add', req.body);
+  const { employee } = req.app.get('models');
+  employee.create(req.body).then(data => {
+    res.redirect('/employees');
+  });
 };
